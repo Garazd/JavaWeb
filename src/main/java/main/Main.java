6567ab3d6d3a63b881086aff18a8973204458caf
Garazd <GarazdVZ@gmail.com>
2015-12-16 17:30:22 +0200

    Update Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,16 +4,13 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
-import servlets.MirrorServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        MirrorServlet mirrorServlet = new MirrorServlet();
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(mirrorServlet), "/mirror/*");
         context.addServlet(new ServletHolder(allRequestsServlet), "/*");
 
         Server server = new Server(8080);
