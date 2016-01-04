7478bff6a4543df2fbb86406474dc70609e9f7f5
Garazd <GarazdVZ@gmail.com>
2016-01-04 12:54:38 +0200

    Added SignInServlet and SignUpServlet

--- L2.1 Authorization/src/main/java/main/Main.java
+++ L2.1 Authorization/src/main/java/main/Main.java
@@ -9,6 +9,8 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.SessionsServlet;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 import servlets.UsersServlet;
 
 public class Main {
@@ -18,6 +20,14 @@ public class Main {
         accountService.addNewUser(new UserProfile("admin"));
         accountService.addNewUser(new UserProfile("test"));
 
+        SignInServlet signInServlet = new SignInServlet();
+        SignUpServlet signUpServlet = new SignUpServlet();
+
+        ServletContextHandler signIn = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        signIn.addServlet(new ServletHolder(signInServlet), "/signin/*");
+        ServletContextHandler signUp = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        signUp.addServlet(new ServletHolder(signUpServlet), "/signup/*");
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
         context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
@@ -26,12 +36,13 @@ public class Main {
         resource_handler.setResourceBase("public_html");
 
         HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[]{resource_handler, context});
+        handlers.setHandlers(new Handler[]{resource_handler, context, signIn, signUp});
 
         Server server = new Server(8080);
         server.setHandler(handlers);
 
         server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
         server.join();
     }
 }
