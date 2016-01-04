81c577d0fd080e30919257c1c8d0a42d0b0fee5f
Garazd <GarazdVZ@gmail.com>
2016-01-04 12:43:09 +0200

    Added SignUpServlet

---
+++ L2.1 Authorization/src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SignUpServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=utf-8");
+
+        PrintWriter out = response.getWriter();
+        out.println(request.getParameter("Authorized"));
+
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
