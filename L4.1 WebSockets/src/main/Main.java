0f55c75cb68b0806e6906bbb0f2b5f5df9281c79
Garazd <GarazdVZ@gmail.com>
2016-01-25 15:32:29 +0200

    Refactoring main and pom.xml

--- L4.1 WebSockets/src/main/Main.java
+++ L4.1 WebSockets/src/main/Main.java
@@ -24,6 +24,7 @@ public class Main {
         server.setHandler(handlers);
 
         server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
         server.join();
     }
 }
