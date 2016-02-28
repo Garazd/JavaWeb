9b0e5dddbc7ab11d1b3f8771018905b35b1474f0
Garazd <GarazdVZ@gmail.com>
2016-02-28 22:08:40 +0200

    Added Main class

---
+++ L6.5 Resource/src/main/java/example/Main.java
@@ -0,0 +1,49 @@
+package example;
+
+import resourceServer.ResourceServer;
+import resourceServer.ResourceServerController;
+import resourceServer.ResourceServerControllerMBean;
+import resourceServer.ResourceServerImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.HomePageServlet;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+
+public class Main {
+    static final Logger logger = LogManager.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws Exception {
+
+        ResourceServerImpl resourceServer = new ResourceServer("Garazd", 5);
+
+        ResourceServerControllerMBean serverStatistics = new ResourceServerController(resourceServer);
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("Admin:type=ResourceServerController");
+        mbs.registerMBean(serverStatistics, name);
+
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new HomePageServlet(resourceServer)), HomePageServlet.PAGE_URL);
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("static");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
+
+        server.start();
+        logger.info("Server started");
+        server.join();
+    }
+}
